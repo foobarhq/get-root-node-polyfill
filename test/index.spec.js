ecc6467bf3eaf7874b7b59eb0f9cfe49679c791d
Guylian Cox <gc@pictawall.com>
2018-02-16 12:35:33 +0100

    test: Add cross-realmn test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var chai = require('chai');
 var dirtyChai = require('dirty-chai');
 var getRootNode = require('../index');
@@ -5,57 +7,182 @@ var getRootNode = require('../index');
 chai.use(dirtyChai);
 var expect = chai.expect;
 
+function supportsShadowRoot() {
+  return typeof Element.prototype.attachShadow === 'function';
+}
+
 describe('Node.prototype.getRootNode', function describeGetRootNode() {
 
-  it('returns the root of detached trees', function testGetRootDetached() {
-    var rootNode = document.createElement('div');
-    var childNode = document.createElement('div');
-    var descendantNode = document.createElement('div');
+  var realmns = [];
 
-    rootNode.appendChild(childNode);
-    childNode.appendChild(descendantNode);
+  function createRealmn() {
+    var iframe = document.createElement('iframe');
+    iframe.src = window.location;
+    document.body.appendChild(iframe);
 
-    expect(getRootNode.call(descendantNode)).to.equal(rootNode);
-  });
+    realmns.push(iframe);
 
-  it('returns the root of attached trees', function testGetRootDetached() {
-    var rootNode = document.createElement('div');
-    var childNode = document.createElement('div');
-    var descendantNode = document.createElement('div');
+    return iframe.contentWindow;
+  }
 
-    document.body.appendChild(rootNode);
-    rootNode.appendChild(childNode);
-    childNode.appendChild(descendantNode);
-
-    expect(getRootNode.call(descendantNode)).to.equal(document);
-  });
-
-  it('returns itself if it is the root', function testGetRoot() {
-    var detachedNode = document.createElement('div');
-
-    expect(getRootNode.call(detachedNode)).to.equal(detachedNode);
-    expect(getRootNode.call(document)).to.equal(document);
-  });
-
-  if (typeof Element.prototype.attachShadow === 'function') {
+  function createShadowRoot(document) {
     var node = document.createElement('div');
+    document.body.appendChild(node);
     var shadowRoot = node.attachShadow({ mode: 'open' });
     var shadowNode = document.createElement('div');
     shadowRoot.appendChild(shadowNode);
 
-    it('works on shadow roots', function testGetRoot() {
-      expect(getRootNode.call(shadowNode)).to.equal(shadowRoot);
-    });
-
-    it('composed option defaults to false', function testGetRoot() {
-      expect(getRootNode.call(shadowNode))
-        .to.equal(getRootNode.call(shadowNode, { composed: false }));
-    });
-
-    it('composed option returns the shadow root\'s host\'s root', function testGetRoot() {
-      expect(getRootNode.call(shadowNode, { composed: true })).to.equal(node);
-    });
+    return { root: shadowRoot, node: shadowNode };
   }
+
+  afterEach(function cleanupRealmns() {
+    for (var i = 0; i < realmns.length; i++) {
+      realmns[i].remove();
+    }
+
+    realmns = [];
+  });
+
+  describe('returns the root of detached trees', function testGetRootDetached() {
+
+    function run(document) {
+      var rootNode = document.createElement('div');
+      var childNode = document.createElement('div');
+      var descendantNode = document.createElement('div');
+
+      rootNode.appendChild(childNode);
+      childNode.appendChild(descendantNode);
+
+      expect(getRootNode.call(descendantNode)).to.equal(rootNode);
+    }
+
+    it('current realmn', function inRealmn() {
+      run(window.document);
+    });
+
+    it('cross-realmn', function outRealmn() {
+      run(createRealmn().document);
+    });
+  });
+
+  describe('returns the root of attached trees', function testGetRootAttached() {
+
+    function run(document) {
+      var rootNode = document.createElement('div');
+      var childNode = document.createElement('div');
+      var descendantNode = document.createElement('div');
+
+      document.body.appendChild(rootNode);
+      rootNode.appendChild(childNode);
+      childNode.appendChild(descendantNode);
+
+      expect(getRootNode.call(descendantNode)).to.equal(document);
+    }
+
+    it('current realmn', function inRealmn() {
+      run(window.document);
+    });
+
+    it('cross-realmn', function outRealmn() {
+      run(createRealmn().document);
+    });
+  });
+
+  describe('returns itself if it is the root', function testGetSelf() {
+
+    function run(document) {
+      var detachedNode = document.createElement('div');
+
+      expect(getRootNode.call(detachedNode)).to.equal(detachedNode);
+      expect(getRootNode.call(document)).to.equal(document);
+    }
+
+    it('current realmn', function inRealmn() {
+      run(window.document);
+    });
+
+    it('cross-realmn', function outRealmn() {
+      run(createRealmn().document);
+    });
+  });
+
+  describe('works with shadow roots', function testGetShadow() {
+
+    function run(document) {
+      var elems = createShadowRoot(document);
+      expect(getRootNode.call(elems.node)).to.equal(elems.root);
+    }
+
+    it('current realmn', function inRealmn() {
+      if (!supportsShadowRoot()) {
+        this.skip();
+        return;
+      }
+
+      run(window.document);
+    });
+
+    it('cross-realmn', function outRealmn() {
+      if (!supportsShadowRoot()) {
+        this.skip();
+        return;
+      }
+
+      run(createRealmn().document);
+    });
+  });
+
+  describe('"composed" option returns the shadow root\'s host\'s root', function testGetShadowsRoot() {
+
+    function run(document) {
+      var elems = createShadowRoot(document);
+      expect(getRootNode.call(elems.node, { composed: true })).to.equal(document);
+    }
+
+    it('current realmn', function inRealmn() {
+      if (!supportsShadowRoot()) {
+        this.skip();
+        return;
+      }
+
+      run(window.document);
+    });
+
+    it('cross-realmn', function outRealmn() {
+      if (!supportsShadowRoot()) {
+        this.skip();
+        return;
+      }
+
+      run(createRealmn().document);
+    });
+  });
+
+  describe('"composed" option defaults to false', function testComposed() {
+
+    function run(document) {
+      var elems = createShadowRoot(document);
+      expect(getRootNode.call(elems.node)).to.equal(getRootNode.call(elems.node, { composed: false }));
+    }
+
+    it('current realmn', function inRealmn() {
+      if (!supportsShadowRoot()) {
+        this.skip();
+        return;
+      }
+
+      run(window.document);
+    });
+
+    it('cross-realmn', function outRealmn() {
+      if (!supportsShadowRoot()) {
+        this.skip();
+        return;
+      }
+
+      run(createRealmn().document);
+    });
+  });
 });
 
 describe('implement.js', function describeGetRootNode() {
